075bd128ca86e9ca11bdc152740bfa0ea1e6a07a
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2014-02-11 17:12:35 -0500

    First Commit
    First commit for this project

---
+++ Bitwall/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Bitwall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "BitWall!";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(114, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BITWALL!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Bitwall.Properties.Resources.TheS;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 118);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Bitwall/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+
+
+namespace Bitwall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class wallMessage
+        {
+            public string Address;
+            public double Amount;
+            public int Confirmations;
+
+            public wallMessage()
+            {
+            }
+
+            public wallMessage(string Address, string moneyz, string confs)
+            {
+                Address = Address.Substring(Address.IndexOf(": \"") + 3);
+                Address = Address.Substring(0, Address.IndexOf("\""));
+                this.Address = Address;
+
+                moneyz = moneyz.Substring(moneyz.IndexOf(": ") + 2);
+                moneyz = moneyz.Substring(0, moneyz.IndexOf(","));
+                this.Amount = double.Parse(moneyz);
+
+                confs = confs.Substring(confs.IndexOf(": ") + 2);
+                this.Confirmations = int.Parse(confs);
+            }
+
+        }
+
+        System.Threading.Timer cTimer;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cTimer = new System.Threading.Timer(chkWall);
+            cTimer.Change(0, System.Threading.Timeout.Infinite);
+        }
+
+        private void chkWall(object state)
+        {
+            //get all addresses associated with bitwall
+            ProcessStartInfo psi = new ProcessStartInfo(@"S:\Bitcoin\daemon\bitcoind.exe", "-rpcuser=USER -rpcpassword=PASSWORD listreceivedbyaddress");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            string everything;
+            using (Process process = Process.Start(psi))
+            {
+                //
+                // Read in all the text from the process with the StreamReader.
+                //
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    //discard the first line
+                    everything = reader.ReadToEnd();
+                }
+            }
+
+            String[] raw = everything.Split(new String[] { "\r\n" }, StringSplitOptions.None);
+            List<wallMessage> allMsg = new List<wallMessage>();
+            if (((raw.Length - 3) % 6) == 0)
+            {
+                for (int i = 1; i < (raw.Length - 2); i += 6)
+                {
+                    if (raw[i + 2].Contains("Bitwall"))
+                    {
+                        
+
+                        allMsg.Add(new wallMessage(raw[i + 1], raw[i + 3], raw[i+4]));
+                    }
+                }
+            }
+
+            String str = @"server=localhost;database=DATABASE;userid=USER;password=PASSWORD;";
+            MySqlConnection con = null;
+            MySqlDataReader rr = null;
+
+            try
+            {
+                //if it's in the system get it's info
+                con = new MySqlConnection(str);
+                con.Open();
+
+                foreach (wallMessage aMsg in allMsg)
+                {
+                    if (aMsg.Confirmations > 3)
+                    {
+                        //do mysql stuff
+                        String cmdText = "UPDATE data SET value=@value, paid=@paid WHERE address='" + aMsg.Address + "'";
+                        MySqlCommand cmd = new MySqlCommand(cmdText, con);
+                        cmd.Prepare();
+
+                        cmd.Parameters.AddWithValue("@paid", 1);
+                        cmd.Parameters.AddWithValue("@value", aMsg.Amount);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                //Log exception
+                //Display Error message
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close(); //close the connection
+                }
+            } //remember to close the connection after accessing the database
+
+
+            cTimer.Change(60000, System.Threading.Timeout.Infinite);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+            }
+            else
+            {
+                this.ShowInTaskbar = true;
+            }
+        }
+    }
+}
